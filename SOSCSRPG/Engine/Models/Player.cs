01d360c32980936e9f9f0b67ffd4286f908e9ac6
swanj21 <swanj21@gmail.com>
2018-02-06 17:23:25 -0500

    Added the ViewModel folder with GameSession inside. Modified Player class, making it public

--- SOSCSRPG/Engine/Models/Player.cs
+++ SOSCSRPG/Engine/Models/Player.cs
@@ -8,5 +8,11 @@ namespace Engine.Models
 {
     class Player
     {
+        public string playerName { get; set; }
+        public string charClass { get; set; }
+        public int hitPoints { get; set; }
+        public int expPoints { get; set; }
+        public int level { get; set; }
+        public int gold { get; set; }
     }
 }
---
+++ SOSCSRPG/Engine/ViewModels/GameSession.cs
@@ -0,0 +1,25 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    /* This is used to manage the session as the player is 
+       playing the game. 
+    */
+    class GameSession
+    {
+        Player curPlayer { get; set; }
+
+        public GameSession()
+        {
+            curPlayer = new Player();
+
+            curPlayer.playerName = "Jerry";
+            curPlayer.gold = 100500;
+        }
+    }
+}
