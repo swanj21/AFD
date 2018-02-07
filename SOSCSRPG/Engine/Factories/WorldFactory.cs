2f579553d3ebeb8d86d6b861502596bfe6c51b64
swanj21 <swanj21@gmail.com>
2018-02-07 14:33:44 -0500

    Actually added the worldfactory and images, along with the location and world classes

---
+++ SOSCSRPG/Engine/Factories/WorldFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    // class WorldFactory is by default an internal class
+    internal class WorldFactory
+    {
+        internal World CreateWorld()
+        {
+            World world = new World();
+            world.AddLocation(-2, -1, "Farmer's Field",
+                "There are rows of corn growing here, with giant rats hiding between them.",
+                "/Engine;component/Image/Locations/FarmFields.png");
+            world.AddLocation(-1, -1, "Farmer's House",
+                "This is the house of your neighbor, Farmer Ted.",
+                "/Engine;component/Images/Locations/Farmhouse.png");
+            world.AddLocation(0, -1, "Home", 
+                "This is your home.", 
+                "/Engine;component/Images/Locations/Home.png");
+            world.AddLocation(-1, 0, "Trading Shop",
+                "The shop of Susan, the trader.",
+                "/Engine;component/Images/Locations/Trader.png");
+            world.AddLocation(0, 0, "Town Square",
+                "There is a fountain here.",
+                "/Engine;component/Images/Locations/TownSquare.png");
+            world.AddLocation(1, 0, "Town Gate",
+                "There is a gate here, protecting the town from giant spiders.",
+                "/Engine;component/Images/Locations/TownGate.png");
+            world.AddLocation(2, 0, "Spider Forest",
+                "The trees in this forest are covered with spider webs.",
+                "/Engine;component/Images/Locations/SpiderForest.png");
+            world.AddLocation(0, 1, "Herbalist's hut",
+                "You see a small hut, with plants drying from the roof.",
+                "/Engine;component/Images/Locations/HerbalistsHut.png");
+            world.AddLocation(0, 2, "Herbalist's garden",
+                "There are many plants here, with snakes hiding(not very well) behind them.",
+                "/Engine;component/Images/Locations/HerbalistsGarden.png");
+
+            return world;
+        }
+    }
+}
---
+++ SOSCSRPG/Engine/Models/World.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class World
+    {
+        private List<Location> locations = new List<Location>();
+
+        internal void AddLocation(int xCoord, int yCoord, string name, string desc, string imageName)
+        {
+            Location loc = new Location();
+
+            loc.XCoordinate = xCoord;
+            loc.YCoordinate = yCoord;
+            loc.Name = name;
+            loc.Description = desc;
+            loc.ImageName = imageName;
+
+            locations.Add(loc);
+        }
+
+        public Location LocationAt(int xCoord, int yCoord)
+        {
+            foreach(Location loc in locations)
+            {
+                if (loc.XCoordinate == xCoord && loc.YCoordinate == yCoord)
+                {
+                    return loc;
+                }
+            }
+
+            return null;
+        }
+    }
+}
